223f5c768472405da6ccb5f51101d9e6e559d490
weiwei <weiwei_work_bj@163.com>
2025-12-11 17:40:20 +0800

    增加Jenkins文件22

---
+++ src/main/java/com/ww/App.java
@@ -0,0 +1,13 @@
+package com.ww;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int a;
+        a=10;
+        int b= 100/0;
+        System.out.println(b);
+    }
+}
