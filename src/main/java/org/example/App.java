bd3d461062005b92bfa8ca6b2fe06d0016463a59
weiwei <weiwei_work_bj@163.com>
2025-12-11 17:05:31 +0800

    初始化版本

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int a;
+        a=10;
+        int b= 100/0;
+        System.out.println(b);
+    }
+}
